1063f5a8b99f2fc6dbd39be41943f1b9cf26dc33
MyNameIsNotBob1121 <88466314+MyNameIsNotBob1121@users.noreply.github.com>
2021-08-11 16:39:43 -0400

    Update roast.js

--- Commands/MemberTools/roast.js
+++ Commands/MemberTools/roast.js
@@ -3,13 +3,14 @@ const { prefix } = require('../../SuperSecretData/config.json');
 module.exports.run = async (client, message, args) => {
 
 let roasts = [
-  "If everyone were cookies, you'd be oatmeal raisin",
-  "Placeholder 2"
+  "If everyone were cookies, ${message.mentions.members.first().username} would be the stale six month old oatmeal raisin on the back shelf",
+  "I'm using ${message.mentions.members.first().username.username} as a placeholder because that's all they can do"
+  "if there was a laziness contest, ${message.mentions.members.first().username} wouldn't even get a rank because they can't be bothered to get off their couch"
 ];
 //The different types of roasts
 let roast = roasts[Math.floor(Math.random() * roasts.length)];
- //I'm assuming this means that it choses one out of these choices, got the format from https://www.codegrepper.com/code-examples/javascript/select+random+object+from+array+javascript
-  message.channel.send(`${roast}.`);
+ //I'm assuming this means that it randomly choses one out of these choices, got the format from https://www.codegrepper.com/code-examples/javascript/select+random+object+from+array+javascript
+  message.channel.send("${roast}.");
   //And this sends it out    
   
 }
@@ -18,7 +19,7 @@ module.exports.help = {
     name: 'roast',
     aliases: ['insult'],
     description: 'Roasts the user medium rare',
-    usage: `${prefix}<roast> <user:OPTIONAL>`,
+    usage: `${prefix}<roast> <@user>`,
     cooldown: 1,
     args: false, 
     legacy: false,
