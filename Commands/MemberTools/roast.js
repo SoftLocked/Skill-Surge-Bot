43b1bbfc715b77326cab7aff8066fa57223c55bf
MyNameIsNotBob1121 <88466314+MyNameIsNotBob1121@users.noreply.github.com>
2021-08-11 14:35:34 -0400

    Update roast.js

--- Commands/MemberTools/roast.js
+++ Commands/MemberTools/roast.js
@@ -1,1 +1,25 @@
+const Discord = require('discord.js');
+const { prefix } = require('../../SuperSecretData/config.json');
+module.exports.run = async (client, message, args) => {
+
+var myArray = [
+  "If everyone were cookies, you'd be oatmeal raisin",
+  "Placeholder 2"];
+
+var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
+
+  const embed = new Discord.MessageEmbed()
+   .setColor('7BC3EF')
+  
+ 
+  if(!args[0]) return message.channel.send('randomItem=myArray')
+
+module.exports.help = {
+    name: 'roast',
+    aliases: ['command'],
+    description: 'Roasts the user medium rare',
+    usage: `${prefix}<Command> <Usage>`,
+    cooldown: 1,
+    args: false, 
+    legacy: false,
 
