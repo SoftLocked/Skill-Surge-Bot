92c73f127ab2707ba9cd3d78ec68f4179eb4a250
Tensor <46503752+TensileTensor@users.noreply.github.com>
2021-09-20 19:19:36 -0700

    Update roast.js

--- commands/Entertainment/roast.js
+++ commands/Entertainment/roast.js
@@ -2,28 +2,24 @@ const Discord = require('discord.js');
 const { prefix } = require('../../SuperSecretData/config.json');
 module.exports.run = async (client, message, args) => {
 
-let roasts = [
-  'If everyone were cookies, ${message.mentions.members.first().username} would be the stale six month old oatmeal raisin on the back shelf',
-  'I'm using ${message.mentions.members.first().username} as a placeholder because that's all they can do'
-  'if there was a laziness contest, ${message.mentions.members.first().username} wouldn't even get a rank because they can't be bothered to get off their couch'
-];
-//The different types of roasts
-if (args[0] === '<@!' + message.mentions.members.first().id + '>') {
-let roast = roasts[Math.floor(Math.random() * roasts.length)];
- //I'm assuming this means that it randomly choses one out of these choices, got the format from https://www.codegrepper.com/code-examples/javascript/select+random+object+from+array+javascript
-  message.channel.send('${roast}.');
-  //And this sends it out    
-  
-}
-//can't forget the curly brackets
+    let roasts = [
+        `If everyone were cookies, ${message.mentions.members.first().user.username} would be the stale six month old oatmeal raisin on the back shelf.`,
+        `I'm using ${message.mentions.members.first().user.username} as a placeholder because that's all they can do.`,
+        `if there was a laziness contest, ${message.mentions.members.first().user.username} wouldn't even get a rank because they can't be bothered to get off their couch.`
+    ];
+    //The different types of roasts
+    if (args[0] === '<@!' + message.mentions.members.first().id + '>') {
+        //Randomly chooses one of the roasts in the array and sends it out
+        let roast = roasts[Math.floor(Math.random() * roasts.length)];
+        message.channel.send(`${roast}`);
+    }
 }
 module.exports.help = {
     name: 'roast',
     aliases: ['insult'],
-    description: 'Roasts the user medium rare',
+    description: 'Roasts the user',
     usage: `${prefix}<roast> <@user>`,
     cooldown: 1,
-    args: true, 
+    args: true,
     legacy: false,
 }
-//I SAID, DON'T FORGET THE CURLY BRACKETS DAMMIT 
